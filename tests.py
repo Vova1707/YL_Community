6757a9c9cf7115b9db226a151abd843983c51a1b
«Владимир <kondrahinvov@yandex.ru>
2025-05-13 13:35:35 +0300

    Тесты pytest

---
+++ tests.py
@@ -0,0 +1,17 @@
+from app import app
+from db_session import global_init
+
+
+
+class TestView:
+    def setup_method(self):
+        global_init(app.config['DATABASE_URI'])
+        app.testing = True
+        self.client = app.test_client()
+
+    def test_connection(self):
+        response = self.client.get('/')
+        assert response.status_code == 200
+
+    def teardown_method(self):
+        print('\nТесты завершены')
