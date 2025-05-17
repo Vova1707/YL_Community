7f875740c89d700487e29f93680243e477cec5d6
«Владимир <kondrahinvov@yandex.ru>
2025-05-17 13:21:28 +0300

    S6

--- db_session.py
+++ db_session.py
@@ -12,11 +12,12 @@ __factory = None
 def global_init(db_file):
     global __factory
 
-    if __factory:
+    if __factory != None:
         return
 
-    conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
-    logging.info(f"Подключение к базе данных по адресу {conn_str}")
+    #conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
+    conn_str = 'sqlite:///opt/build/db/db.sqlite'
+    print(f"Подключение к базе данных по адресу {conn_str}")
 
     engine = sa.create_engine(conn_str, echo=False)
     __factory = orm.sessionmaker(bind=engine)
@@ -26,6 +27,6 @@ def global_init(db_file):
     SqlAlchemyBase.metadata.create_all(engine)
 
 
-def create_session() -> Session:
+def create_session():
     global __factory
     return __factory()
